540dad3071fa341ebdd5e9ceb664e4e13807fdfe
Janek <Janek@JANEK-PC>
2016-10-19 20:53:20 +0200

    Add project files.

---
+++ Pogoda/GetHtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Pogoda
+{
+    class GetHtml
+    {
+        public string html;
+        public void side()
+        {
+            //pobieranie kodu html ze strony
+            try
+            {
+                string url = "http://www.twojapogoda.pl/polska/zachodniopomorskie/koszalin";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+                html = sr.ReadToEnd();
+                sr.Close();
+            }catch(Exception ea)
+            {
+                MessageBox.Show(ea.ToString());
+            }
+            //Zapis do pliku w katalogu Pogoda
+
+            try
+            {
+                System.IO.File.WriteAllText(@"C:/Users/Janek/Documents/Visual Studio 2015/Projects/Pogoda/web/html.txt", html);
+            }catch(Exception er)
+            {
+                MessageBox.Show(er.ToString());
+            }
+        }
+    }
+}
---
+++ Pogoda/RegEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+//klasa filtrujaca strone html
+
+namespace Pogoda
+{
+    class RegEx
+    {
+        public string text = "asd 20 bsdf 21 345";
+        public string patern = @"\d\d";
+        public void wypisz(){
+        Match m = Regex.Match(text, patern);
+            Console.WriteLine(m.Value);
+    }
+    }
+}
