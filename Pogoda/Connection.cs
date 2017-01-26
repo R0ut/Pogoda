8335e857de9d9a5f66395799f5727036c0eb06d5
R0ut <kkaacczzoorr@gmail.com>
2017-01-26 18:21:27 +0100

    v.1.1

---
+++ Pogoda/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pogoda
+{
+    class Connection
+    {
+        public bool Internet()
+        {
+            try
+            {
+                var ping = new System.Net.NetworkInformation.Ping();
+
+                var result = ping.Send("www.google.pl");
+
+                if (result.Status != System.Net.NetworkInformation.IPStatus.Success)
+                    return false;
+                else return true;
+
+            }
+            catch (PingException ad)
+            {
+                MessageBox.Show("Internet connection problem");
+                return false;
+            }
+        }
+    }
+}
